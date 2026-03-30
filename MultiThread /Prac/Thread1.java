f83cf16e547175b16cafbea8bb75481fd179c3e6
Sachin2006-Z <premy9299@gmail.com>
2026-03-30 22:26:50 +0530

    Added

---
+++ MultiThread /Prac/Thread1.java
@@ -0,0 +1,15 @@
+package com.MultiThread.Prac;
+
+public class Thread1 implements Runnable {
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Who are you");
+
+    }
+}
+
