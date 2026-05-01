65189ef39598f415e75d95efac0aa808eb7d6b43
Sachin2006-Z <premy9299@gmail.com>
2026-05-01 22:13:10 +0530

    Added

---
+++ JDBC/JdbcExample.java
@@ -0,0 +1,13 @@
+package org.example;
+import java.sql.*;
+
+public class JdbcExample {
+    public static void main(String[] args) {
+
+        JDBC_Connections o1=new JDBC_Connections();
+
+        o1.mydatabase();
+
+
+    }
+}
